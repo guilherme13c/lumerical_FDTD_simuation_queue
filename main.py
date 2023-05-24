588458d2174370483c9233b2b39504ee73f914e1
guilherme caporali <guilhermecaporali504@gmail.com>
2023-05-24 10:17:08 -0300

    ready to test

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
-import sys, subprocess, queue, time
+import sys
+import subprocess
+import queue
+import time
+import json
 from datetime import timedelta
 
-LUMERICAL_EXECUTABLE = ""
+LUMERICAL_EXECUTABLE = json.load(open('env.json'))["LUMERICAL_EXECUTABLE"]
+
 
 def simulate(simulation_file):
     start_time = time.monotonic()
     print(f"Started: {simulation_file}")
-    
+
     subprocess.run([LUMERICAL_EXECUTABLE, "-run", simulation_file])
-    
+
     end_time = time.monotonic()
-    print(f"Finished: {simulation_file} \t{timedelta(seconds=(end_time-start_time))}")
+    print(
+        f"Finished: {simulation_file} \t{timedelta(seconds=(end_time-start_time))}")
+
 
 simulations_queue = queue.Queue()
 
 for arg in sys.argv[1:]:
     simulations_queue.put(arg)
-    
+
 while (not simulations_queue.empty()):
     sim = simulations_queue.get()
     simulate(sim)
